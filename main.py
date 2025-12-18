5061bc1e999f1581164088f0c71224e259e2dbf3
Thomas Bullen <bullen@umich.edu>
2025-12-18 10:43:59 -0500

    :wq

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import sklearn
+import xgboost as xgb
+
+
+df = pd.read_csv('necessaryfiles/healthcare-dataset-stroke-data.csv')
+print(df.head())
+
